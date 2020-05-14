3bc6711f04605863efe121bce6a84e0069486a09
Karashily <ahmed.elkarashily@gmail.com>
2020-05-14 04:26:22 +0200

    id_ex reg

--- decode/onebitreg.vhd
+++ decode/onebitreg.vhd
@@ -1,21 +1,21 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity reg is
-    generic (n:integer := 32);
-  port(d : in std_logic_vector(n-1 downto 0);
+entity onebitreg is
+  port(d : in std_logic;
       clk, rst, load: in std_logic;
-      q: out std_logic_vector(n-1 downto 0));
-end reg;
+      q: out std_logic);
+end onebitreg;
 
-architecture arch of reg is
+architecture arch of onebitreg is
   begin
     process(clk, rst)
       begin
         if(rst ='1') then
-          q <= (others=>'0');
+          q <= '0';
         elsif rising_edge(clk) and load = '1' then
           q <= d;
         end if;
     end process;
   end architecture;
+
